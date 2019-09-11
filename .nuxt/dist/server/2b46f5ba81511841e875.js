16bc33a1079ee73bc1a9c1bb21abb9b109fe61aa
feiy <you@example.com>
2019-09-11 21:04:52 +0800

    update .gitignore

--- .nuxt/dist/server/2b46f5ba81511841e875.js
+++
@@ -1,2 +0,0 @@
-exports.ids=[9],exports.modules={182:function(t,e,r){"use strict";r.r(e);var n={},o=r(3);var component=Object(o.a)(n,function(){var t=this.$createElement;return(this._self._c||t)("div",[this._ssrNode("1111")])},[],!1,function(t){},null,"741d7604");e.default=component.exports}};
-//# sourceMappingURL=2b46f5ba81511841e875.js.map

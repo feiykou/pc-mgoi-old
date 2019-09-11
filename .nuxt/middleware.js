6d4ccd8cbb22c8f4db7f91d8a454548f2a677301
feiy <feiy@163.com>
2019-09-11 21:24:50 +0800

    添加吸附，banner导航dot动画

--- .nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['column'] = require('..\\middleware\\column.js');
-middleware['column'] = middleware['column'].default || middleware['column']
-
-export default middleware

461221a20269f533399500a70e6c07a5d7c32c79
feiy <feiy@163.com>
2019-09-11 20:53:54 +0800

    添加吸附，banner导航dot动画

--- .nuxt/middleware.js
+++ .nuxt/middleware.js
@@ -1,5 +1,8 @@
 const middleware = {}
 
+middleware['column'] = require('..\\middleware\\column.js');
+middleware['column'] = middleware['column'].default || middleware['column']
+
 middleware['nav'] = require('..\\middleware\\nav.js');
 middleware['nav'] = middleware['nav'].default || middleware['nav']
 
